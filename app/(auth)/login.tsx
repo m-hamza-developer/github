0f225a2c3f5cf0967bcd1fb765291430128a3817
hamzavrox <princecherry241@gmail.com>
2026-08-11 12:55:44 +0500

    add login funcatonality

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,29 @@
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login Screen</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(auth)/login.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({  
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
