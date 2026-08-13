280a88ef422bc7770dd483d79babe9fa8e63d854
hamzavrox <princecherry241@gmail.com>
2026-08-13 11:15:38 +0500

    add text InputBox.tsx

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function InputBox() {
     return (
         <View>
-            <Text>InputBox</Text>
+            <Text>InputBox update</Text>
         </View>
     );
 }
