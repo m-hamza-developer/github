61f444fd34d80604e4f2cc149fa8e228bda6de18
hamzavrox <princecherry241@gmail.com>
2026-08-17 11:45:05 +0500

    Add some text on signup.tsx page

---
+++ app/(auth)/signup.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Signup() {
+    return (
+        <View className="flex min-h-screen flex-col items-center justify-center py-2">
+            <Text className="text-4xl font-bold mb-4">Sign Up page here</Text>
+        </View>
+    );
+}
