d199c47fe422db465da92aa311f8d6d70bd5ade5
hamzavrox <princecherry241@gmail.com>
2026-08-17 12:27:13 +0500

    add text on signup page.

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Signup() {
     return (
         <View className="flex min-h-screen flex-col items-center justify-center py-2">
-            <Text className="text-4xl font-bold mb-4">Sign Up</Text>
+            <Text className="text-4xl font-bold mb-4">Sign Up Page</Text>
         </View>
     );
 }
